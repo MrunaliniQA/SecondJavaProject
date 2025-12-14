d1d50122de5d3992313aec1730e25d9b26db4342
Mrunalini Ukey <mrunaliniukey1@gmail.com>
2025-12-14 19:22:14 +0530

    2nd commit for Addition class after value change

--- src/Addition.java
+++ src/Addition.java
@@ -1,7 +1,7 @@
 public class Addition {
     public static void main(String[] args) {
         int a=10;
-        int b=20;
+        int b=50;
         int c;
 
         System.out.println("The result will be : "+(c=a+b));
