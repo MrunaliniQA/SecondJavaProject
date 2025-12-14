082afbaa4de459acfe2e2ecf4175fb6f639f701d
Mrunalini Ukey <mrunaliniukey1@gmail.com>
2025-12-14 19:27:35 +0530

    3rd commit with b value print

--- src/Addition.java
+++ src/Addition.java
@@ -4,6 +4,8 @@ public class Addition {
         int b=50;
         int c;
 
-        System.out.println("The result will be : "+(c=a+b));
+        System.out.println("The result of addition will be : "+(c=a+b));
+
+        System.out.println("The Value of  : "+b);
     }
 }
