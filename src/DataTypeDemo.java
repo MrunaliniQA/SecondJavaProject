85649f11ecb034945b301300b47e9da82572836d
Mrunalini_U <mrunaliniukey1@gmail.com>
2025-12-19 19:17:14 +0530

    DataType program

---
+++ src/DataTypeDemo.java
@@ -0,0 +1,46 @@
+public class DataTypeDemo {
+    public static void main(String[] args) {
+  //Numeric Data Type
+        int a= 100;
+        int b=200;
+        System.out.println("The value of a and b is "+a+" "+b);
+        System.out.println("The addition of a and b is "+(a+b));
+        System.out.println(a+b);
+
+        byte by=125;
+        System.out.println("Byte value is "+by);
+
+        short sh=3454;
+        System.out.println("short value is "+sh);
+
+        long ln=26532654645446l;
+        System.out.println("The value of long is "+ln);
+
+        // Decimal Number
+        float fl=4333.789f;
+        System.out.println("The float value is "+fl);
+
+        double dl=63543645.786768;
+        System.out.println("The value of double is "+dl);
+
+        //String Value
+
+        String name="John";
+        System.out.println("The String is "+name);
+
+        char ch='B';
+        System.out.println("The grade of student is "+ch);
+
+        String str="A";
+        System.out.println("The String is "+str);
+
+        boolean bol=true;
+        System.out.println("The output is "+bol);
+
+        //String bl=false;        // Invalid as string is always in double quote
+        //System.out.println(bl);
+
+    }
+
+
+}
